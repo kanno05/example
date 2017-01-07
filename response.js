816c3857553740ed68145bbb66b131dfcce2f909
kanno <kanno.kohei05@gmail.com>
2017-01-07 23:45:26 +0900

    commit

---
+++ response.js
@@ -0,0 +1,93 @@
+const LINE_CHANNEL_ACCESS_TOKEN = 'Key';
+
+var request = require('request');
+
+module.exports = class dietitian {
+    static replyRecommendation(replyToken){
+        var headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+        }
+        var body = {
+            replyToken: replyToken,
+            messages: [{
+                type: 'text',
+                text: 'カレーはどうですか？'
+            }]
+        }
+        var url = 'https://api.line.me/v2/bot/message/reply';
+        request({
+            url: url,
+            method: 'POST',
+            headers: headers,
+            body: body,
+            json: true
+        });
+    }
+
+    static replyTotalCalorie(replyToken, foodList){
+        var totalCalorie = 0;
+        for (var food of foodList){
+            totalCalorie += food.calorie;
+        }
+
+        var headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+        }
+        var body = {
+            replyToken: replyToken,
+            messages: [{
+                type: 'text',
+                text: 'カロリーは合計' + totalCalorie + 'kcal'
+            }]
+        }
+        var url = 'https://api.line.me/v2/bot/message/reply';
+        request({
+            url: url,
+            method: 'POST',
+            headers: headers,
+            body: body,
+            json: true
+        });
+    }
+
+    static askWhichFood(replyToken, foodList){
+        var headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+        }
+        var body = {
+            replyToken: replyToken,
+            messages: [{
+                type: 'template',
+                altText: 'どの食品が最も近いですか？',
+                template: {
+                    type: 'buttons',
+                    text: 'どの食品が最も近いですか？',
+                    actions: []
+                }
+            }]
+        }
+        for (var food of foodList){
+            body.messages[0].template.actions.push({
+                type: 'postback',
+                label: food.food_name,
+                data: JSON.stringify(food)
+            });
+
+            if (body.messages[0].template.actions.length == 4){
+                break;
+            }
+        }
+
+        var url = 'https://api.line.me/v2/bot/message/reply';
+        request({
+            url: url,
+            method: 'POST',
+            headers: headers,
+            body: body,
+            json: true
+        });
+    }
+}
